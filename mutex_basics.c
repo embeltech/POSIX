bcb1a9473e6f7d8b1f225239d5e2f15ec15c8685
embeltech <56225547+embeltech@users.noreply.github.com>
2024-08-07 22:44:42 +0530

    Update mutex_basics.c

--- mutex_basics.c
+++ mutex_basics.c
@@ -1,23 +1,26 @@
+/*
+*   C compiler: gcc -o out thread_basic.c -lpthread
+*   C++ compiler: g++ -o out thread_basic.c -lpthread
+*/
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h> //sleep
 
 pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+int shared_variable = 0;
 
-int shared_counter = 0;
 void * threadFunc(void * arg)
 {
-    char * msg = (char *) arg;
-    int count = 5;
-    while(count)
+    int * id = (int *) arg;
+    int i = 0;
+    for(i=0; i<20; i++)
     {
-        pthread_mutex_lock(&mtx); //critical section starts
-        shared_counter++;
-        printf("%s| Count=%d | shared_counter= %d\n",msg,count,shared_counter); 
-        pthread_mutex_unlock(&mtx); //critical section ends
-        
-        count--;
-        sleep(1);
+        pthread_mutex_lock(&mtx);  
+        shared_variable++;  
+        printf("Thread %d| shared_variable=%d\n",*id,shared_variable); 
+        pthread_mutex_unlock(&mtx);
+
+        usleep(100000);
     }
     pthread_exit(NULL);
 }
@@ -25,14 +28,14 @@ void * threadFunc(void * arg)
 int main()
 {
      pthread_t thread1, thread2;
-     char *message1 = "Thread 1";
-     char *message2 = "Thread 2";
+     int id1 = 1;
+     int id2 = 2;
      int  iret1, iret2;
 
     /* Create independent threads each of which will execute function */
 
-     iret1 = pthread_create( &thread1, NULL, threadFunc, (void*) message1);
-     iret2 = pthread_create( &thread2, NULL, threadFunc, (void*) message2);
+     iret1 = pthread_create( &thread1, NULL, threadFunc, (void*) &id1);
+     iret2 = pthread_create( &thread2, NULL, threadFunc, (void*) &id2);
 
      printf("Thread 1 returns: %d\n",iret1);
      printf("Thread 2 returns: %d\n",iret2);
@@ -46,4 +49,3 @@ int main()
     
      return 0;
 }
-

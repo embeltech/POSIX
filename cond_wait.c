8f212c86c25c6e2c3f58b68c0f3deac82bce16a0
embeltech <56225547+embeltech@users.noreply.github.com>
2024-08-07 22:58:34 +0530

    Update cond_wait.c

--- cond_wait.c
+++ cond_wait.c
@@ -1,92 +1,76 @@
 /*
-* https://rextester.com/l/cpp_online_compiler_gcc
+*   C compiler: gcc -o out thread_basic.c -lpthread
+*   C++ compiler: g++ -o out thread_basic.c -lpthread
 */
-
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h> //sleep
 
 pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+
 pthread_mutex_t mtx_cond = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond_var = PTHREAD_COND_INITIALIZER;
 
-/*
- A condition variable must always be associated with a mutex to avoid a race condition 
- created by one thread preparing to wait and another thread which may signal the condition 
- before the first thread actually waits on it resulting in a deadlock. 
-*/
+int shared_variable = 0;
 
-int shared_counter = 0;
 void * threadFunc1(void * arg)
 {
-    char * msg = (char *) arg;
-    int count = 10;
-    while(count)
+    int * id = (int *) arg;
+    int i = 0;
+    for(i=0; i<20; i++)
     {
-        //-------------- Wait Condition-----------------
-        // make Thread 1 wait till Thread 2 increaments shared_counter till 5. then both threads continue.
-        pthread_mutex_lock(&mtx_cond);
-        if(shared_counter < 5)
+        pthread_mutex_lock(&mtx);  
+        shared_variable++;  
+        printf("Thread %d| shared_variable=%d\n",*id,shared_variable); 
+        pthread_mutex_unlock(&mtx);
+
+        if(i==9)
         {
-            printf("T1 waiting\n");
-            pthread_cond_wait(&cond_var, &mtx_cond);
-        }        
-        pthread_mutex_unlock(&mtx_cond);
-        
-        
-        pthread_mutex_lock(&mtx); //critical section starts
-        shared_counter++;
-        printf("%s| Count=%d | shared_counter= %d\n",msg,count,shared_counter); 
-        pthread_mutex_unlock(&mtx); //critical section ends
-        
-        count--;
-        usleep(100000);//IMP as other thread should get chance to run. else single thread will run all the time
+            printf("Signaling Thread2\n");
+            pthread_mutex_lock(&mtx_cond);
+            pthread_cond_signal(&cond_var);
+            pthread_mutex_unlock(&mtx_cond);
+        }
+
+        usleep(100000);
     }
-    printf("%s exiting\n",msg);
     pthread_exit(NULL);
 }
 
 void * threadFunc2(void * arg)
 {
-    char * msg = (char *) arg;
-    int count = 10;
-    while(count)
+    pthread_mutex_lock(&mtx_cond);
+    pthread_cond_wait(&cond_var,&mtx_cond);
+    pthread_mutex_unlock(&mtx_cond);
+
+    printf("Wait Over for Thread2\n");
+
+    int * id = (int *) arg;
+    int i = 0;
+    for(i=0; i<20; i++)
     {
-        //-------------- signal Condition-----------------
-        // make Thread 1 wait till Thread 2 increaments shared_counter till 5. then both threads continue.
-        pthread_mutex_lock(&mtx_cond);
-        if(shared_counter == 5)
-        {
-            printf("T2 signalling\n");
-            pthread_cond_signal(&cond_var);
-        }        
-        pthread_mutex_unlock(&mtx_cond);
-        
-        
-        pthread_mutex_lock(&mtx); //critical section starts
-        shared_counter++;
-        printf("%s| Count=%d | shared_counter= %d\n",msg,count,shared_counter); 
-        pthread_mutex_unlock(&mtx); //critical section ends
-        
-        count--;
-        usleep(100000); //IMP as other thread should get chance to run. else single thread will run all the time
+        pthread_mutex_lock(&mtx);  
+        shared_variable++;  
+        printf("Thread %d| shared_variable=%d\n",*id,shared_variable); 
+        pthread_mutex_unlock(&mtx);
+
+        usleep(100000);
     }
-    printf("%s exiting\n",msg);
     pthread_exit(NULL);
 }
+
 
 int main()
 {
      pthread_t thread1, thread2;
-     char *message1 = "Thread 1";
-     char *message2 = "Thread 2";
+     int id1 = 1;
+     int id2 = 2;
      int  iret1, iret2;
 
     /* Create independent threads each of which will execute function */
 
-     iret1 = pthread_create( &thread1, NULL, threadFunc1, (void*) message1);
-     sleep(1);
-     iret2 = pthread_create( &thread2, NULL, threadFunc2, (void*) message2);
+     iret1 = pthread_create( &thread1, NULL, threadFunc1, (void*) &id1);
+     iret2 = pthread_create( &thread2, NULL, threadFunc2, (void*) &id2);
 
      printf("Thread 1 returns: %d\n",iret1);
      printf("Thread 2 returns: %d\n",iret2);
@@ -100,4 +84,3 @@ int main()
     
      return 0;
 }
-
